b7fa42aa4680105d99d7628f06ba02624628d1ef
luizhakan.javascript <luizhakan.uefibiosxx@gmail.com>
2022-02-08 01:09:11 -0300

    Exemplos da aula 1

---
+++ tipos-de-dados/type-boolean.js
@@ -0,0 +1,15 @@
+/* 
+false --> falso
+true --> verdadeiro
+*/
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+// console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura"
+const texto2 = 'alura'
+
+console.log(texto1 === texto2)
---
+++ tipos-de-dados/type-number.js
@@ -0,0 +1,23 @@
+//type number
+
+const primeiroNumero: 1;
+const segundoNumero: 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN --> Not A Number (Não é um número)
+
+const alura = "Alura"
+const opalura = (alura * primeiroNumero)
+
+console.log(opalura)
