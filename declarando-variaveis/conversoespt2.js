594ef96b838c34a08218ae03b7ee28f487b5660c
luizhakan.javascript <luizhakan.uefibiosxx@gmail.com>
2022-02-08 09:41:15 -0300

    códigos var e conversões

---
+++ declarando-variaveis/conversoespt2.js
@@ -0,0 +1,20 @@
+// string
+
+// let telefone = 40028922;
+/* 
+console.log("O telefone é " + String(telefone)) teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+*/
+
+// ou podemos usar
+
+// console.log("O telefone é " + telefone.toString()) o .toString() é uma outra forma para  fazer essa conversão, que é mais parecida com outras linguagens de programação.
+
+/*
+let usuarioConectado = false;
+console.log(String(usuarioConectado)) teremos a conversão da booleana para string, nesse caso teremos uma string “false”.
+
+usuarioConectado = true;
+console.log(String(usuarioConectado))
+agora teremos uma string “true”.
+*/
+
