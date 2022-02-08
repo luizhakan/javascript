919c02f803d99d6d5719d989f7c9ae330f997028
luizhakan.javascript <luizhakan.uefibiosxx@gmail.com>
2022-02-08 08:49:59 -0300

    códigos var e conversões

---
+++ declarando-variaveis/conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado
+// booleano
+
+// conversão implícita
+const numero = 456;
+const numeroString = Number("456a");
+
+// Number()
+// String()
+console.log(numero + numeroString)
+
+
+// conversão explícita
+
+/*
+= - atribuir valor a uma variável
+== - comparar o valor da variável
+=== - comparar valor e tipo da variável
+*/  
