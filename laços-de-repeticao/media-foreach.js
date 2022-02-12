22b4e024de2911ac3bd8b41de3001283ab155557
luizhakan.javascript <luizhakan.uefibiosxx@gmail.com>
2022-02-11 23:17:45 -0300

    for, forEach, etc

--- laços-de-repeticao/media-foreach.js
+++ laços-de-repeticao/media-foreach.js
@@ -18,3 +18,7 @@ let media = somaDasNotas/notas.length
 console.log(media)
 
 // a função forEach tem o nome de callback e vai ser executada para cada elemento do array, sendo que pode receber de 1 a 3 argumentos; o elemento, o índice e o array atual, respectivamente.
+
+// o forEach permite usar outras funções dentro ou fora dela
+
+// Enquanto o método forEach não tem um retorno, o método map() pode retornar um array se a função informada tiver um return em seu escopo.
