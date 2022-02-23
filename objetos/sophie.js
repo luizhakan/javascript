681b9080e4b71b29362c95d93ecf844a55658da4
luizhakan.javascript <luizhakan.uefibiosxx@gmail.com>
2022-02-22 21:19:00 -0300

    cadastro de informações pessoais

---
+++ objetos/sophie.js
@@ -0,0 +1,13 @@
+// ATENÇÃO, DADOS GERADOS ALEATÓRIAMENTE, USADO PARA FINS DE EXEMPLO!!!
+const sophie = {
+    nome: "Sophie Raquel",
+    dataDeNascimento: "26/07/1967",
+    rg: 130158641,
+    email: "sophie-raquel@net.com",
+    telefone: 81985073119,
+    cidade: "recife",
+    estado: "pernambuco"
+}
+
+console.log(sophie)
+console.log(sophie.nome,sophie.dataDeNascimento,sophie.email)
