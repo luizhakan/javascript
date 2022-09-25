8d6f312a4990dc22839131f7845682c3d3ef1a51
luizhakan <luizhakan2@protonmail.com>
2022-09-25 19:59:05 -0300

    algoritmo insertionSort

---
+++ algoritmos-ordenacao/insertionSort/listaLivros.js
@@ -0,0 +1,24 @@
+const listaLivros = [
+  {
+    nome: "As Crônicas de Gelo e Fogo",
+    preco: 45,
+  },
+  {
+    nome: "O Senhor dos Anéis",
+    preco: 15,
+  },
+  {
+    nome: "Fundação",
+    preco: 30,
+  },
+  {
+    nome: "O Hobbit",
+    preco: 20,
+  },
+  {
+    nome: "Guerra e Paz",
+    preco: 50,
+  },
+];
+
+module.exports = listaLivros;
